84ad155bca0b09bb5a4989353abe784da345a42d
Manasa <Manasa@DESKTOP-LOG4SRC>
2025-03-09 14:00:29 +0530

    Added Statements --Manasa Priya J

--- MVN1/src/test/java/com/tissolve/testscript/ChatsModule.java
+++ MVN1/src/test/java/com/tissolve/testscript/ChatsModule.java
@@ -1,5 +1,7 @@
 package com.tissolve.testscript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
@@ -8,7 +10,11 @@ import com.tissolve.generic.BaseClass;
 public class ChatsModule extends BaseClass {
 	@Test
 	public void demo() {
-		Reporter.log("HI", true);
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		Reporter.log(driver.getTitle(), true);
+		driver.quit();
 	}
 	
 }
